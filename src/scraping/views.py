4403af4cbf739fc184f87d6bfbeecd85ec5782da
skaylife <worldskaynet@gmail.com>
2020-06-28 20:02:46 +0800

    move home.html in app scraping.

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -3,4 +3,4 @@ from .models import Vacancy
 
 def home_view(request):
     qs = Vacancy.objects.all()
-    return render(request, 'home.html', {'objects_list': qs})
+    return render(request, 'scraping/home.html', {'objects_list': qs})
