d84f38c6441182b5e1bb70509c0a8b9e578fb529
skaylife <worldskaynet@gmail.com>
2020-06-28 20:00:27 +0800

    settings Template inheritance, add base.html, add home.html refactor. Request BaseDate, and look home.html.

--- src/scraping/views.py
+++ src/scraping/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from .models import Vacancy
 
-# Create your views here.
+def home_view(request):
+    qs = Vacancy.objects.all()
+    return render(request, 'home.html', {'objects_list': qs})
