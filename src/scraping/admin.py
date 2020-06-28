4c9e400a838dfab5bbe177fba86f1cfd5aa758f9
skaylife <worldskaynet@gmail.com>
2020-06-28 18:02:31 +0800

    add Class Vanansy.

--- src/scraping/admin.py
+++ src/scraping/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import City, language
+from .models import City, language, Vacancy
 
 
 admin.site.register(City)
 admin.site.register(language)
+admin.site.register(Vacancy)
