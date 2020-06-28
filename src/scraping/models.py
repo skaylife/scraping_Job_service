6ab98c5bb00519ad857ad01c34cb38fc01f66562
skaylife <worldskaynet@gmail.com>
2020-06-28 17:39:51 +0800

    Admin part / Create app & create requests in BaseDate & create function Slug translate name cyrillic in translit.

---
+++ src/scraping/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+from scraping.utils import from_cyrillic_to_eng
+
+class City(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name='Название города',
+                            unique=True)
+    
+    slug = models.CharField(max_length=50,
+                            blank=True,
+                            unique=True)
+    class Meta:
+        verbose_name = 'Название города'
+        verbose_name_plural = 'Название городов'
+    
+    def __str__(self):
+        return self.name
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+            super().save(*args, **kwargs)
+    
+    
+    
+    
+class language(models.Model):
+    name = models.CharField(max_length=50,
+                            verbose_name='Язык программирования',
+                            unique=True)
+
+    slug = models.CharField(max_length=50,
+                            blank=True,
+                            unique=True)
+
+    class Meta:
+        verbose_name = 'Язык программирования'
+        verbose_name_plural = 'Языки программирования'
+
+    def __str__(self):
+        return self.name
+    
+    
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = from_cyrillic_to_eng(str(self.name))
+            super().save(*args, **kwargs)
